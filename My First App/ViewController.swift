a33b3c11066b435f500154e26fccd91aedd57053
elnatan <elnatan@ravtech.co.il>
2020-01-16 17:06:02 +0200

    Initial Commit

---
+++ My First App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My First App
+//
+//  Created by admin on 16/01/2020.
+//  Copyright © 2020 AM Kirsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
