7bd61bd549457819e993e13c1a3ef91f0d4463ff
Avraham Kirsch <etrog95@gmail.com>
2020-09-29 13:28:36 +0300

    first commit

--- My First App/ViewController.swift
+++ My First App/ViewController.swift
@@ -5,14 +5,32 @@
 //  Created by admin on 16/01/2020.
 //  Copyright © 2020 AM Kirsch. All rights reserved.
 //
-
+//
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var yourname: UITextField!
+    @IBAction func ButtonClicked(_ sender: Any) {
+        
+        print("button clicked!")
+        
+        if let name = yourname.text{
+            label.text = "Hello " + name + "!"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
